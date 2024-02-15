b72699332d089046d31b6e43b917298131c31add
imran2024bd <imranitbd34@gmail.com>
2024-02-15 16:55:01 +0600

    Change Home Screen To Playground By Click

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,3 +1,20 @@
 function play(){
-    console.log('play start');
-}
+     // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+     //     homeSection.classList.add('hidden');
+     //     // console.log(homeSection.classList)
+     
+     const homeSection = document.getElementById('home-screen');
+     //  console.log(homeSection.classList);
+     homeSection.classList.add('hidden');
+
+     // show the playground
+     const playgroundSection = document.getElementById('play-ground');
+    //  console.log('playground section');
+    // console.log(playgroundSection.classList);
+    playgroundSection.classList.remove('hidden');
+    }
+
+
+    function play() {
+        hideElementById('home-screen');
+    }
